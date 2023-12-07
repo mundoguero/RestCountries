c89b09d2da74206f1af33e8fd7093a759b832376
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2023-12-07 14:27:47 -0300

    Adding CountryList View Model
    Split the CountryList rules in a separate View Model file

--- RestCountries/Views/ContentView.swift
+++ RestCountries/Views/ContentView.swift
@@ -8,55 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var countries = [Country]()
-    @State private var searchTerm = ""
-    
-    var filteredCountries: [Country] {
-        guard !searchTerm.isEmpty else { return countries }
-        return countries.filter { $0.name.common.localizedCaseInsensitiveContains(searchTerm) }
-    }
-    
+    @StateObject private var viewModel = CountryListViewModel()
+
     var body: some View {
         NavigationStack {
-            List (filteredCountries, id: \.self) { country in
+            List(viewModel.filteredCountries, id: \.self) { country in
                 HStack(spacing: 10) {
-                    Text (country.name.common)
+                    Text(country.name.common)
                         .bold()
-                        .minimumScaleFactor (0.5)
-                    
+                        .minimumScaleFactor(0.5)
+
                     Spacer()
-                    
+
                     Text("\(country.population)")
                         .bold()
                         .foregroundColor(.gray)
                 }
             }
-            
-            .onAppear(){
-                getCountriesData()
+            .onAppear {
+                viewModel.getCountriesData()
             }
             .navigationTitle("Countries")
-            .searchable(text: $searchTerm, prompt: "Search Country")
+            .searchable(text: $viewModel.searchTerm, prompt: "Search Country")
         }
     }
 }
 
-extension ContentView {
-    func getCountriesData() {
-        Network().getCountries { (result) in
-            switch result {
-            case.success(let countries):
-                DispatchQueue.main.async {
-                    self.countries = countries
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-}
 
 #Preview {
     ContentView()
